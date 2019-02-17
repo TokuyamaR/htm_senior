f8067c5b4f659bfbd3b5af0455da84895ee3d2ed
徳山 来人 <tokuyamaraido@team-lab.com>
2019-02-17 17:31:20 +0900

    190217 [modify] font-size, JS of humbarger-menu

--- js/app.js
+++ js/app.js
@@ -5,9 +5,15 @@ $(function () {
         $('.js-float-menu').toggleClass('float-active', $(this).scrollTop() > targetHeight);
     });
 
-    // SPメニュー
+    // SPメニューを開く処理
     $('.js-toggle-sp-menu').on('click', function () {
         $(this).toggleClass('active');
+        $('.js-toggle-sp-menu-target').toggleClass('active');
+    });
+
+    // SPメニューを閉じる処理
+    $('.menu-item').on('click', function () {
+        $('.menu-trigger').toggleClass('active');
         $('.js-toggle-sp-menu-target').toggleClass('active');
     });
 });
